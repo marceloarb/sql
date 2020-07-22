05a6ebd0edc60aa1964257af28d658d33ff6a7e8
marceloarb <59586505+marceloarb@users.noreply.github.com>
2020-07-22 16:29:53 -0500

    Instance changed and user as well

--- aws-rds.sql
+++ aws-rds.sql
@@ -3,9 +3,9 @@ it and AWS is very important to learn.
 The steps I took was:
 Loggin to my aws account.
 Select RDS -> create database -> mysql -> free tier.
-Create a user = admin and password = marcelobarbosa;
+Create a user = marcelo and password = marcelobarbosa;
 The rest was a default and my database was created after submit.
 Go to dashboard -> databases -> select the database you want.
-Here is my instance: aws-rds.cneslkijzdx1.us-east-1.rds.amazonaws.com
+Here is my instance: bonussql.cneslkijzdx1.us-east-1.rds.amazonaws.com
 
 
